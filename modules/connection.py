a81a343279eafec447da8a3751f2f33b9eedd377
Liam <liam.estell@outlook.com>
2024-05-22 15:15:32 -0400

    initial commit

---
+++ modules/connection.py
@@ -0,0 +1,58 @@
+from math import floor
+import pyshark.packet.packet as pypacket
+
+class Connection:
+
+    def __init__(self, name: str, user_ip: str, server_ips: list[str], start_time: float):
+
+        # Setup variables
+        self.names = [name]
+        self.user_ip = user_ip
+        self.server_ips = server_ips
+
+        # Constant starting variables
+        self.end_time = 0
+        self.volume = 0
+        self.packet_count = 0
+        
+        # Set start time
+        self.start_time = floor(start_time)
+
+    def accept_packet(self, packet: pypacket.Packet) -> None:
+
+        # update end time
+        self.end_time = floor(float(packet.sniff_timestamp))
+
+        # probably increase length correctly TODO: Check this
+        self.volume += len(packet)
+
+        # Increase packet count
+        self.packet_count += 1
+
+    def expand_server_ips(self, new_ips: list[str]) -> None:
+
+        # Add new ips to the server ips list
+        for ip in new_ips:
+            if ip not in self.server_ips:
+                self.server_ips.append(ip)
+
+    def expand_name(self, new_name: str) -> None:
+
+        # Add the new name if necessary
+        if new_name not in self.names:
+            self.names.append(new_name)
+
+    # Return whether or not a packet is part of our connection
+    def is_ours(self, packet: pypacket.Packet) -> bool:
+
+        return (packet.ip.src in self.server_ips)
+    
+    # Check if a packet should be considered a new connection for us
+    def is_new_connection(self, packet: pypacket.Packet) -> bool:
+
+        # If the query name is the same, but the time is 5 minutes after our end time, return true
+        return packet.dns.qry_name in self.names and ((floor(float(packet.sniff_timestamp)) - (self.end_time if self.end_time > self.start_time else self.start_time)) >= 300)
+    
+    def __str__(self) -> str:
+
+        return f"{self.names}<br />-----Start Time: {self.start_time}<br />-----End Time: {self.end_time}<br />-----Connection Length (sec): {self.end_time - self.start_time}<br />-----Size Down: {self.volume}<br />-----Server Ips: {self.server_ips}<br />"
---
+++ modules/gui.py
@@ -0,0 +1,54 @@
+class GUI:
+
+    def __init__(self, modules: list, fp: str):
+
+        self.modules = modules
+        self.fp = fp
+
+
+        return
+    
+    def updateGUI(self) -> None:
+
+        out = open(self.fp, "w")
+
+        # Get all connections
+        all_connections = []
+        for module in self.modules:
+            for connection in module.connections:
+                all_connections.append(connection)
+            for connection in module.dead_connections:
+                all_connections.append(connection)
+
+        # Sort connections by user_ip
+        users = {}
+
+        for connection in all_connections:
+
+            uip = connection.user_ip
+
+            if not uip in users:
+                users[uip] = []
+
+            users[uip].append(connection)
+
+        # TODO initial setup
+        out.write("<meta http-equiv=\"refresh\" content = \"5; URL=file:///home/kali/Documents/output.html\" />") # Automatically refresh the page
+
+        # Generate each column by user ip
+        for uip in users:
+
+            # TODO Setup user
+
+            # Get the connection list for this user
+            conn_list = users[uip]
+
+            # For each connection
+            for conn in conn_list:
+
+                # TODO: setup connection
+                out.write(str(conn))
+
+        # Finish the method
+        out.close()
+        return
